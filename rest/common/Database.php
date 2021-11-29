b558253939dddf7201b23d0946e1d13b44c32f50
monmonplaza <monmon.plaza@gmail.com>
2021-11-29 15:11:27 +0800

    done

--- rest/common/Database.php
+++ rest/common/Database.php
@@ -6,11 +6,11 @@ class Database {
     private $password;
     public function getConnection() {
         $host = "localhost";
-        //$databaseName = "dbvgfndluxiatg"; 
-        $databaseName = "db_eternal"; 
-       // $username = "ugxv5milyju80"; 
-        $username = "root"; 
-       // $password = "Tr2Na,+kI9c$"; 
+        $databaseName = "dbvgfndluxiatg"; 
+        // $databaseName = "db_fca_students"; 
+        $username = "ugxv5milyju80"; 
+       // $username = "root"; 
+        $password = "Tr2Na,+kI9c$"; 
         $password = ""; 
         $mysqli = new mysqli($host, $username, $password, $databaseName);
         if($mysqli->connect_error) {
